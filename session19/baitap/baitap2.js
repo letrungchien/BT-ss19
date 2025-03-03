b57329b74b79c81c50bd47103f609d0ce8488f1d
letrungchien <letrungchien195@gmail.com>
2025-03-03 15:48:11 +0700

    Update baitap2.js

--- session19/baitap/baitap2.js
+++ session19/baitap/baitap2.js
@@ -1,7 +1,6 @@
-let text =prompt("mời bạn nhập");
-let KiemTra = text.indexOf("0","1","2","3","4","5","6","7","8","9");
-if(KiemTra == -1){
-    alert("ký tự "+text+" là chữ cái");
-}else{
-    alert("ký tự "+text+" không phải là chữ cái");
+let text = String(prompt("nhập ký tự bất kì: "));
+if(text.includes("0")|| text.includes("1")|| text.includes("2")|| text.includes("3")|| text.includes("4")|| text.includes("5")|| text.includes("6")|| text.includes("7")|| text.includes("8")|| text.includes("9")){
+    alert(text + " không phải chữ cái");
+} else {
+    alert("ký tự " + text + " là chữ cái");
 }
