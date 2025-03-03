219cfa0db3c421a2a3407719baaca03cbaa3c614
letrungchien <letrungchien195@gmail.com>
2025-03-03 16:10:34 +0700

    Update baitap7.js

--- session19/baitap/baitap7.js
+++ session19/baitap/baitap7.js
@@ -1,9 +1,5 @@
-let Number1 = Number(prompt("nhập số thứ nhất: "));
-let Number2 = Number(prompt("nhập số thứ hai: "));
-let Number3 = Number(prompt("nhập số thứ ba: "));
-let max = Math.max(Number1,Number2,Number3);
-if(!isNaN(Number1) && !isNaN(Number2) && !isNaN(Number3)){
-    alert("số lớn nhất trong 3 số là: " + max);
-} else {
-    alert("vui lòng nhập số hợp lệ!");
-}
+let a = Number(prompt("nhập số thứ nhất: "));
+let b = Number(prompt("nhập số thứ hai: "));
+let c = Number(prompt("nhập số thứ ba: "));
+let max = (a>b)?((a>c)?a:c):((b>c)?b:c);
+ alert("số lớn nhất trong 3 số là "+max);
